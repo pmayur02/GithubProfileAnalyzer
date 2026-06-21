7048c6eaad9c0fb869c7f8ccb6f716aa5f3f484f
pmayur02 <mayurpansare2000@gmail.com>
2026-06-21 23:29:05 +0530

    unignored certfile.

--- Backend/Database/dbConnection.js
+++ Backend/Database/dbConnection.js
@@ -1,9 +1,6 @@
 const mysql = require("mysql2");
 require("dotenv").config();
 const fs = require("fs")
-const path = require("path");
-
-const certFile = path.join(__dirname,"../Certs/isrgrootx1.pem")
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
@@ -11,7 +8,7 @@ const pool = mysql.createPool({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   ssl:{
-    ca: fs.readFileSync(certFile),
+    ca: fs.readFileSync(process.env.CA),
     rejectUnauthorized: false
   }
 });
