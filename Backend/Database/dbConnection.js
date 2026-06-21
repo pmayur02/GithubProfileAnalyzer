6fb160eb5aafcfc8dd42f35731874de86c61b6c0
pmayur02 <mayurpansare2000@gmail.com>
2026-06-21 19:04:58 +0530

    added db cert file.

--- Backend/Database/dbConnection.js
+++ Backend/Database/dbConnection.js
@@ -1,11 +1,15 @@
 const mysql = require("mysql2");
 require("dotenv").config();
+const fs = require("fs")
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  ssl:{
+    ca: fs.readFileSync(process.env.CA)
+  }
 });
 
 const db = pool.promise();
