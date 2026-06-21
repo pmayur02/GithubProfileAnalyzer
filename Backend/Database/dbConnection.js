5d69d7e02ea75cf6bcb66db1950d2a93bd22a12c
pmayur02 <mayurpansare2000@gmail.com>
2026-06-21 19:16:13 +0530

    rejectUnauthorized: false add setting  for db cert.

--- Backend/Database/dbConnection.js
+++ Backend/Database/dbConnection.js
@@ -8,7 +8,8 @@ const pool = mysql.createPool({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   ssl:{
-    ca: fs.readFileSync(process.env.CA)
+    ca: fs.readFileSync(process.env.CA),
+    rejectUnauthorized: false
   }
 });
 
