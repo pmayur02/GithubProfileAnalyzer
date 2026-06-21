fc0f92746ad006289b02d1c8a331becbf42d59fb
pmayur02 <mayurpansare2000@gmail.com>
2026-06-21 23:14:39 +0530

    change code to read cert file.

--- Backend/Database/dbConnection.js
+++ Backend/Database/dbConnection.js
@@ -1,6 +1,9 @@
 const mysql = require("mysql2");
 require("dotenv").config();
 const fs = require("fs")
+const path = require("path");
+
+const certFile = path.join(__dirname,"../Certs/isrgrootx1.pem")
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
@@ -8,7 +11,7 @@ const pool = mysql.createPool({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   ssl:{
-    ca: fs.readFileSync(process.env.CA),
+    ca: fs.readFileSync(certFile),
     rejectUnauthorized: false
   }
 });
